63a52c5e1266397cc0a1c1e5c5cb2f8212192eff
Bartelus <wilroed@gmail.com>
2024-09-25 11:30:51 +0200

    setup html og css

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ console.log('Hello!');
 
 let imagedisplay = document.querySelector(image.html)
 
+let detteBildet = i
+
+
 function imagepage ()
 {
-
 }
 
 menytrypp.addEventListener("click", imagepage)
